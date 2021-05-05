637b4de5390fb85e5930f5211cc1c65a9d0b42aa
Benoit0298 <benoit.lorcy@gmail.com>
2021-05-05 19:03:56 +0200

    Aded html js and css

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,4 @@
-/**
+/*
  * @Author: Thibault Napoléon <Imothep>
  * @Company: ISEN Yncréa Ouest
  * @Email: thibault.napoleon@isen-ouest.yncrea.fr
@@ -16,14 +16,12 @@
 // \param data The data associated with the request.
 function ajaxRequest(type, url, callback, data = null) {
     let xhr;
-
     // Create XML HTTP request.
     xhr = new XMLHttpRequest();
     if (type == 'GET' && data != null)
         url += '?' + data;
     xhr.open(type, url);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
     // Add the onload function.
     xhr.onload = () => {
         switch (xhr.status) {
@@ -36,19 +34,16 @@ function ajaxRequest(type, url, callback, data = null) {
                 httpErrors(xhr.status);
         }
     };
-
     // Send XML HTTP request.
     xhr.send(data);
 }
-
 //------------------------------------------------------------------------------
 //--- httpErrors ---------------------------------------------------------------
 //------------------------------------------------------------------------------
 // Display an error message accordingly to an error code.
 // \param errorCode The error code (HTTP status for example).
 function httpErrors(errorCode) {
-    let messages =
-    {
+    let messages = {
         400: 'Requête incorrecte',
         401: 'Authentifiez vous',
         403: 'Accès refusé',
@@ -56,7 +51,6 @@ function httpErrors(errorCode) {
         500: 'Erreur interne du serveur',
         503: 'Service indisponible'
     };
-
     // Display error.
     if (errorCode in messages) {
         let errors = document.getElementById("errors");
